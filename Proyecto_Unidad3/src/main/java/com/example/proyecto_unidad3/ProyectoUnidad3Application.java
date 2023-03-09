55bdd31de7b685f36bec176919ae689e7f79fa9e
Jose305 <jaruiz17@espe.edu.ec>
2023-03-09 10:23:05 -0500

    Subiendo proyecto

---
+++ Proyecto_Unidad3/src/main/java/com/example/proyecto_unidad3/ProyectoUnidad3Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_unidad3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUnidad3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoUnidad3Application.class, args);
+    }
+
+}
---
+++ Proyecto_Unidad3/src/test/java/com/example/proyecto_unidad3/ProyectoUnidad3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_unidad3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUnidad3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
